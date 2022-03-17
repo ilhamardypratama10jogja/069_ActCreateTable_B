66542add0fc8a3e0f794f9a62efb4b9391049f83
ilhamardypratama10jogja <ilhamardy10@gmail.com>
2022-03-17 14:46:01 +0700

    Add project files.

---
+++ Create_Tabel/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Create_Tabel
+{
+    class Program
+    {
+        public void CreateTable()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source=ILHAMARDY2001;database=ProdiTI;Integrated Security = TRUE");
+                con.Open();
+
+                SqlCommand cm = new SqlCommand("create table Mahasiswa_Coba (NIM char(12) not null primary key," +
+                     "Nama Varchar(50), Alamat varchar(255), Jenis_Kelamiin char(1))", con);
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Tabel sukses dibuat!");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Oops,sepertinya ada yang salah. " + e);
+                Console.ReadKey();
+
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        static void Main(string[] args)
+        {
+            new Program().CreateTable();
+        }
+    }
+}
+
